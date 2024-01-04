bf5321163cd0524a58efb7eee59d5cfe4c6c26eb
Kim-Onesmus <kimones1122@gmail.com>
2024-01-04 23:09:31 +0300

    Changes

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('id_status', views.IdStatus, name='id_status'),
     path('lost_id', views.LostID, name='lost_id'),
     path('pay_id', views.PayView, name='pay_url'),
-    path('lostpay_id', views.PayView, name='lostpay_url'),
+    path('lostpay_id', views.LostPayView, name='lostpay_url'),
     path('my_id', views.MyIDView, name='myID_url'),
 
     path('about_us', views.AboutUs, name='about_us'),
