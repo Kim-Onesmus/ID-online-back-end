4dbce6fb016fb243293a6e208c9ac54719eec34d
Kim-Onesmus <kimones1122@gmail.com>
2024-02-16 23:42:33 +0300

    updates

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . models import Client
 # Register your models here.
+
+@admin.register(Client)
+class ClientTable(admin.ModelAdmin):
+    list_display = ('first_name', 'sur_name', 'last_name', 'email', 'gender', 'phone', 'date_of_birth')
--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ class Client(models.Model):
     sur_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
-    age = models.CharField(max_length=4, blank=True, null=True)
     gender = models.CharField(max_length=20, blank=True, null=True)
     phone = models.CharField(max_length=13, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
