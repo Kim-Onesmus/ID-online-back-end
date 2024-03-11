952cdcfb5fbaefa05cd5a7af9707c20f28704889
Kim-Onesmus <kimones1122@gmail.com>
2024-03-11 21:54:36 +0300

    updtes

--- app/signals.py
+++ app/signals.py
@@ -17,3 +17,29 @@ def send_status_update_email(sender, instance, **kwargs):
             email_from = settings.EMAIL_HOST_USER
             recipient_list = [instance.client.email, ]
             send_mail(subject, message, email_from, recipient_list)
+
+
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from app.models import Photo, ConfirmationDocument
+from django.core.mail import send_mail
+from django.conf import settings
+
+@receiver(pre_save, sender=Photo)
+@receiver(pre_save, sender=ConfirmationDocument)
+def send_status_update_email(sender, instance, **kwargs):
+    print(f"Signal triggered for {sender.__name__}, Status: {instance.status}")
+    if instance.pk:  # Check if the instance is already in the database
+        old_instance = sender.objects.get(pk=instance.pk)
+        if old_instance.status != instance.status and instance.status == 'approved':
+            subject = 'ID application status updates'
+            message = 'Your ID application status has been updated. \nPlease log in to check the status of your application'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [instance.client.email, ]
+            send_mail(subject, message, email_from, recipient_list)
+            
+photo_instance = Photo.objects.create(status='pending')
+photo_instance.status = 'approved'
+photo_instance.save()
