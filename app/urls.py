52767f083b603d0533800d73fdeaef4f2a1df3f9
Kim-Onesmus <kimones1122@gmail.com>
2024-03-12 11:21:56 +0300

    updates

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 from .views import savePhoto
 
 urlpatterns = [
@@ -26,4 +27,21 @@ urlpatterns = [
     path('update_account', views.UpdateAccount, name='update_account'),
     path('change_password', views.ChangePassword, name='change_password'),
     path('logout', views.LogOut, name='logout'),
+    
+    
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="app/passwordReset/forget-password.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="app/passwordReset/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="app/passwordReset/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="app/passwordReset/password_reset_done.html"), 
+        name="password_reset_complete"),
 ]
