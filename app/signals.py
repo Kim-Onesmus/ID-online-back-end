a027da32afc8451689a6b8f48c0c498f4438ad0d
Kim-Onesmus <kimones1122@gmail.com>
2024-03-12 09:44:18 +0300

    updates

--- app/signals.py
+++ app/signals.py
@@ -8,16 +8,21 @@ from .models import Photo, ConfirmationDocument
 @receiver(pre_save, sender=Photo)
 @receiver(pre_save, sender=ConfirmationDocument)
 def send_status_update_email(sender, instance, **kwargs):
-    # Check if the status field is changed
+    # Check if the status or reason field is changed
     if instance.pk:  # Check if the instance is already in the database
         old_instance = sender.objects.get(pk=instance.pk)
-        if old_instance.status != instance.status and instance.status == 'approved':
+
+        # Check if status or reason is changed
+        if old_instance.status != instance.status or old_instance.reason != instance.reason:
             subject = 'ID application status updates'
-            message = 'Your ID application status has been updated. \nPlease log in to check the status of your application'
+            
+            # Include the reasons in the email message
+            if instance.reason:
+                message = f'Your ID application status has been updated to {instance.status}.\nReason: {instance.reason}\nPlease log in to check the status of your application.'
+            else:
+                message = 'Your ID application status has been updatedto {instance.status}. \nPlease log in to check the status of your application.'
+            
             email_from = settings.EMAIL_HOST_USER
             recipient_list = [instance.client.email, ]
+            
             send_mail(subject, message, email_from, recipient_list)
-
-
-
-
